709286e262224c8fcd7cf04dd81a7fe978784558
fred5697 <fredkuo.mail@gmail.com>
2023-08-28 07:43:58 +0800

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/api/calculate', (req, res) => {
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+
+    if (num1 === undefined || num2 === undefined) {
+        res.status(400).json({ error: 'Both numbers are required in the request body' });
+        return;
+    }
+
+    const result = num1 + num2;
+
+    res.status(200).json({ result: result });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
